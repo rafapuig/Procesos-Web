e8b2a7c19e7c29c1d364e96392349cfa67484326
rafapuig <rafapuig.profe@gmail.com>
2014-11-12 23:42:19 +0100

    inicial

---
+++ App_Code/ProcessData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for ProcessData
+/// </summary>
+public class ProcessData
+{
+    
+    public Int32 Id { get; set; }
+	public Int64 Memory { get; set; }
+	public String Name { get; set; }
+   
+}
